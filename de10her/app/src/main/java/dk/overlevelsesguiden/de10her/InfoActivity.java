a90f7518d45c88a2f167d86efb57d84e0b2175f7
Emil Valentin <atomicpotato8@gmail.com>
2021-03-16 03:02:39 +0100

    Info activity has become functional

--- de10her/app/src/main/java/dk/overlevelsesguiden/de10her/InfoActivity.java
+++ de10her/app/src/main/java/dk/overlevelsesguiden/de10her/InfoActivity.java
@@ -2,16 +2,13 @@ package dk.overlevelsesguiden.de10her;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class InfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent myIntent = new Intent(this, InfoActivity.class);
-        startActivity(myIntent);
+        setContentView(R.layout.activity_info);
     }
 }
