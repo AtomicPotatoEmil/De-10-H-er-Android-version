98d0d76e8c94eda1d7f44fe890f977c2a05485b9
Emil Valentin <atomicpotato8@gmail.com>
2021-03-17 17:41:20 +0100

    Added top bars and clickable icons
    One can now go back and forth between Main and Info Activity

--- de10her/app/src/main/java/dk/overlevelsesguiden/de10her/InfoActivity.java
+++ de10her/app/src/main/java/dk/overlevelsesguiden/de10her/InfoActivity.java
@@ -2,7 +2,10 @@ package dk.overlevelsesguiden.de10her;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+    }
+
+    public void goBackToMainActivity(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- de10her/app/src/main/java/dk/overlevelsesguiden/de10her/MainActivity.java
+++ de10her/app/src/main/java/dk/overlevelsesguiden/de10her/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent myIntent = new Intent(this, InfoActivity.class);
-        startActivity(myIntent);
+
+    }
+
+    public void goToInfoActivity(View view){
+        Intent intent = new Intent(this, InfoActivity.class);
+        startActivity(intent);
     }
 }
