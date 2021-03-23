94e1bd2920ab34022e056552b014b58d2a67c70e
Emil Valentin <atomicpotato8@gmail.com>
2021-03-23 22:35:23 +0100

    App can now save the document information

--- de10her/app/src/main/java/dk/overlevelsesguiden/de10her/MainActivity.java
+++ de10her/app/src/main/java/dk/overlevelsesguiden/de10her/MainActivity.java
@@ -3,15 +3,24 @@ package dk.overlevelsesguiden.de10her;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout savedDocumentsContainer;
+    private ArrayList<Document> documents;
+    private LinearLayout savedDocumentsContainer;
+    private View savedDocumentsView;
 
     /*
     View v;
@@ -22,7 +31,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        SharedPreferences loadPreferences = getSharedPreferences("preference", MODE_PRIVATE);
+        Gson loadGson = new Gson();
+        String loadJson = loadPreferences.getString("document_array", null);
+        Type type = new TypeToken<ArrayList<Document>>() {}.getType();
+        documents = loadGson.fromJson(loadJson, type);
+        if (documents == null){
+            documents = new ArrayList<Document>();
+        }
+
+
+
+
+
+
+        if (getIntent().getStringExtra("document")!= null){
+            Gson gson = new Gson();
+            documents.add(gson.fromJson(getIntent().getStringExtra("document"), Document.class));
+            SharedPreferences savePreferences = getSharedPreferences("preference", MODE_PRIVATE);
+            SharedPreferences.Editor editor = savePreferences.edit();
+            Gson saveGson = new Gson();
+            String saveJson = saveGson.toJson(documents);
+            editor.putString("document_array",saveJson);
+            editor.apply();
+        }
+
+
         savedDocumentsContainer = (LinearLayout)findViewById(R.id.document_button_container);
+
+        for (Document document : documents){
+            savedDocumentsView = getLayoutInflater().inflate(R.layout.saved_documents_view, null, false);
+
+            TextView titleView = (TextView) savedDocumentsView.findViewById(R.id.document_title);
+            titleView.setText(document.getTitle());
+
+            TextView dateView = (TextView) savedDocumentsView.findViewById(R.id.document_date);
+            dateView.setText(document.getDate());
+
+            TextView timeView = (TextView) savedDocumentsView.findViewById(R.id.document_time);
+            timeView.setText(document.getTime());
+
+
+            savedDocumentsContainer.addView(savedDocumentsView);
+        }
         /*
         someList = new int[]{1, 2, 3, 4, 5};
 
--- de10her/app/src/main/java/dk/overlevelsesguiden/de10her/MakeDocumentActivity.java
+++ de10her/app/src/main/java/dk/overlevelsesguiden/de10her/MakeDocumentActivity.java
@@ -3,15 +3,65 @@ package dk.overlevelsesguiden.de10her;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MakeDocumentActivity extends AppCompatActivity {
+
+    private ArrayList<Document> documents;
+
+    private EditText titleEdit;
+    private EditText h1Edit;
+    private EditText h2Edit;
+    private EditText h3Edit;
+    private EditText h4Edit;
+    private EditText h5Edit;
+    private EditText H6Edit;
+    private EditText H7Edit;
+    private EditText H8Edit;
+    private EditText H9Edit;
+    private EditText H10Edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_document);
+
+    }
+
+    public void saveDocument(View view){
+        Document doc = new Document();
+
+        titleEdit = (EditText)findViewById(R.id.document_id);
+        doc.setTitle(titleEdit.getText().toString());
+
+        Calendar date = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd:MM:yyyy");
+        String dateString = dateFormat.format(date.getTime());
+        doc.setDate(dateString);
+
+        Calendar time = Calendar.getInstance();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        String timeString = timeFormat.format(time.getTime());
+        doc.setTime(timeString);
+
+
+        Gson gson = new Gson();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("document", gson.toJson(doc));
+        startActivity(intent);
+
     }
 
     public void goToMainActivity(View view){
